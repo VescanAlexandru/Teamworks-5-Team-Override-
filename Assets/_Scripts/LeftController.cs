318b88d3207fda1e7823dca2d2657c9cf84bebaf
tylermccreary <tjmccreary@gmail.com>
2018-08-14 17:13:33 -0500

    multiplayer with cubes

---
+++ Assets/_Scripts/LeftController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftController : MonoBehaviour
+{
+
+    public MyInputManager inputManager;
+
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        inputManager.SetLeftController(Controller);
+    }
+
+   void Update()
+    {
+        if (Controller.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            inputManager.LeftTouchPadPressUp(Controller.GetAxis());
+        }
+
+        if (Controller.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Debug.Log("LEFT");
+            inputManager.LeftTouchPadPress();
+        }
+
+        if (Controller.GetAxis().x != 0 || Controller.GetAxis().y != 0)
+        {
+            inputManager.SetLeftAxis(Controller.GetAxis());
+        } else
+        {
+            inputManager.ResetLeftAxis();
+        }
+    }
+}
